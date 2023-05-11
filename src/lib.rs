f261430afec35ad80a1c862cfe543f87fa131ac6
Edgar Manukyan <edgar.manukyan@roche.com>
2023-05-11 16:01:07 -0400

    first

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::io::{Error, Read, Write};
+
+pub fn upcase(input: &mut impl Read, output: &mut impl Write) -> Result<(), Error> {
+    let mut buffer = "".to_string();
+
+    input.read_to_string(&mut buffer)?;
+    output.write_all(buffer.to_uppercase().as_bytes())?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::upcase;
+
+    #[test]
+    fn writes_upcased_input_to_output() {
+        let mut output: Vec<u8> = Vec::new();
+
+        upcase(&mut "Hello, world!\n".as_bytes(), &mut output).unwrap();
+        assert_eq!(&output, b"HELLO, WORLD!\n");
+    }
+}
